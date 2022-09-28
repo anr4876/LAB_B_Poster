060aef7e951ee5fd2dc59174cfaf0b10645ee25f
anr4876 <dustjdanr@naver.com>
2022-09-29 05:17:13 +0900

    1st_completion

---
+++ src/swiper.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  autoplay: true,
+  delay: 2000,
+  speed: 400,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
